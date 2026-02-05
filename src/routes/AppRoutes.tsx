cbd46556cb43a9f63825f517082d71b6bff5d33e
Tejas Kedare <tejaskedare2@gmail.com>
2026-02-05 11:38:05 +0530

    updated to lazy routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,36 +1,39 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import Shop from "../pages/Shop";
-import Checkout from "../pages/Checkout";
-import Profile from "../pages/Profile";
-import NotFound from "../pages/NotFound";
-import ProductDetails from "../pages/ProductDetails";
-import Cart from "../pages/Cart";
-import ProfileLayout from "../pages/profile/ProfileLayout";
-import ProfileAccount from "../pages/profile/ProfileAccount";
-import ProfileAddresses from "../pages/profile/ProfileAddresses";
-import ProfileOrders from "../pages/profile/ProfileOrders";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("../pages/Home"));
+const Shop = lazy(() => import("../pages/Shop"));
+const Checkout = lazy(() => import("../pages/Checkout"));
+const ProductDetails = lazy(() => import("../pages/ProductDetails"));
+const Cart = lazy(() => import("../pages/Cart"));
+
+const ProfileLayout = lazy(() => import("../pages/profile/ProfileLayout"));
+const ProfileAccount = lazy(() => import("../pages/profile/ProfileAccount"));
+const ProfileAddresses = lazy(() => import("../pages/profile/ProfileAddresses"));
+const ProfileOrders = lazy(() => import("../pages/profile/ProfileOrders"));
+
+const NotFound = lazy(() => import("../pages/NotFound"));
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/product/:id" element={<ProductDetails />} />
-      <Route path="/cart" element={<Cart />} />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
 
-      <Route path="/profile" element={<ProfileLayout />}>
-  <Route index element={<ProfileAccount />} />
-  <Route path="account" element={<ProfileAccount />} />
-  <Route path="addresses" element={<ProfileAddresses />} />
-  <Route path="orders" element={<ProfileOrders />} />
-</Route>
+        <Route path="/profile" element={<ProfileLayout />}>
+          <Route index element={<ProfileAccount />} />
+          <Route path="account" element={<ProfileAccount />} />
+          <Route path="addresses" element={<ProfileAddresses />} />
+          <Route path="orders" element={<ProfileOrders />} />
+        </Route>
 
-      {/* Fallback route */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   );
 };
 
